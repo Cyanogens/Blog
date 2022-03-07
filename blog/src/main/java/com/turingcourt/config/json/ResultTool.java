4ec42202baba64f8de956feb4e8ced96ea83950d
Cyanogen <2046015981@qq.com>
2022-03-07 21:12:12 +0800

    配置json

---
+++ blog/src/main/java/com/turingcourt/config/json/ResultTool.java
@@ -0,0 +1,24 @@
+package com.turingcourt.config.json;
+
+/**
+ * @author Cyanogen
+ * @description 返回体构造工具
+ * @date 2022/3/7 21:10
+ */
+public class ResultTool {
+    public static JsonResult success() {
+        return new JsonResult(true);
+    }
+
+    public static <T> JsonResult<T> success(T data) {
+        return new JsonResult(true, data);
+    }
+
+    public static JsonResult fail() {
+        return new JsonResult(false);
+    }
+
+    public static JsonResult fail(ResultCode resultEnum) {
+        return new JsonResult(false, resultEnum);
+    }
+}
