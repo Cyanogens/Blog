db87b3f74efcf992a4f4fff007a8950ba9f85653
Cyanogen <2046015981@qq.com>
2022-03-08 23:21:07 +0800

    对json进行更新

--- blog/src/main/java/com/turingcourt/config/json/ResultCode.java
+++ blog/src/main/java/com/turingcourt/config/json/ResultCode.java
@@ -21,16 +21,20 @@ public enum ResultCode {
     /* 用户错误 */
     USER_NOT_LOGIN(2001, "用户未登录"),
     USER_ACCOUNT_EXPIRED(2002, "账号已过期"),
-    USER_CREDENTIALS_ERROR(2003, "密码错误"),
+    USER_CREDENTIALS_ERROR(2003, "账号或密码错误"),
     USER_CREDENTIALS_EXPIRED(2004, "密码过期"),
     USER_ACCOUNT_DISABLE(2005, "账号不可用"),
     USER_ACCOUNT_LOCKED(2006, "账号被锁定"),
     USER_ACCOUNT_NOT_EXIST(2007, "账号不存在"),
     USER_ACCOUNT_ALREADY_EXIST(2008, "账号已存在"),
     USER_ACCOUNT_USE_BY_OTHERS(2009, "账号下线"),
+    USER_ANSWER_ERROR(2010, "密保答案错误"),
+
 
     /* 业务错误 */
-    NO_PERMISSION(3001, "没有权限");
+    NO_PERMISSION(3001, "没有权限"),
+    UPLOAD_ERROR(3002, "图片上传失败");
+
     private Integer code;
     private String message;
 
