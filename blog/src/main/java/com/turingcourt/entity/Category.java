2979d81b96c8cf7359bb0aa65f45ca49f4dbc76f
k <1537006648@qq.com>
2022-03-12 23:27:29 +0800

    3，4完成

--- blog/src/main/java/com/turingcourt/entity/Category.java
+++ blog/src/main/java/com/turingcourt/entity/Category.java
@@ -24,4 +24,5 @@ public class Category implements Serializable {
     */
     private String categoryName;
 
+
 }
--- blog/src/test/java/com/turingcourt/TuringcourtApplicationTests.java
+++ blog/src/test/java/com/turingcourt/TuringcourtApplicationTests.java
@@ -1,13 +1,23 @@
 package com.turingcourt;
 
+import com.turingcourt.dao.CategoryDao;
+import com.turingcourt.entity.Category;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TuringcourtApplicationTests {
 
+    @Autowired
+    private CategoryDao categoryDao;
     @Test
     void contextLoads() {
+        categoryDao.insertCategory(new Category(1L, "first"));
     }
 
 }
+
+
+
+
