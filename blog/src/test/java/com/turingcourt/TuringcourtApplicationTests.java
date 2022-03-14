a851f6abc993d8d6ea7a8e1d89231c4c4e1c6293
Cyanogen <2046015981@qq.com>
2022-03-15 00:24:33 +0800

    优化代码

--- blog/src/test/java/com/turingcourt/TuringcourtApplicationTests.java
+++ blog/src/test/java/com/turingcourt/TuringcourtApplicationTests.java
@@ -1,47 +1,77 @@
 package com.turingcourt;
 
-import com.turingcourt.dao.BlogLikesDao;
-import com.turingcourt.dao.CommentLikesDao;
-import com.turingcourt.dao.UserDao;
-import com.turingcourt.entity.BlogLikes;
-import com.turingcourt.entity.CommentLikes;
+import com.turingcourt.dao.*;
+import com.turingcourt.entity.Blog;
+import com.turingcourt.entity.Category;
 import com.turingcourt.entity.User;
-
+import com.turingcourt.service.UserLoginService;
+import com.turingcourt.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.sql.Date;
 
 @SpringBootTest
 class TuringcourtApplicationTests {
 
     @Autowired
+    UserLoginService userLoginService;
+
+    @Autowired
     UserDao userDao;
+
     @Autowired
-    CommentLikesDao commentLikesDao;
+    CategoryDao categoryDao;
+
     @Autowired
-    BlogLikesDao blogLikesDao;
+    BlogDao blogDao;
+
     @Test
     void contextLoads() {
-        /*Boolean i = userDao.checkAccount("胡歌");
-        System.out.println(i);
-        User user = userDao.getUser(2);
-        System.out.println(user);
-        System.out.println(userDao.getUserByName("胡歌"));
-        System.out.println(userDao.verifyAnswer("胡歌","24"));
-        System.out.println(userDao.verifyAnswer("胡歌","23"));*/
-        //System.out.println(userDao.register(new User(null,"追逐梦想","迪迦","123",true,"男","你好吗","我很好")));
-        //System.out.println(userDao.register(new User(null,"追逐梦","迪","123",true,"男","你好","我很好")));
-        //System.out.println(userDao.register(new User(null,"追逐理想","迦","123",true,"男","你吗","我很好")));
-        /*System.out.println(userDao.update(new User(1,"追梦人","迪迦","123",true,"男","你好吗","我很好")));
+        //User user = new User();
+        //user.setUsername("432");
+        //user.setPassword("123");
+        //user.setProblem("我是谁");
+        //user.setAns("Cya");
+        //user.setNickName("cya");
+        //user.setSex("男");
+        //System.out.println(userLoginService.register(user));
+        //BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        //
+        //User user = userDao.getUser(3);
+        //String password = user.getPassword();
+        //System.out.println("yes?" + passwordEncoder.matches("123", password));
+        //
+        //System.out.println(userLoginService.changePassword("432", "123"));
+        //
+        //System.out.println("password = " + userLoginService.verifyAnswer("432", "Cya"));
 
-        Boolean aBoolean = commentLikesDao.queryLike(new CommentLikes(null, 22L, 33));
-        System.out.println(aBoolean);
-        System.out.println(commentLikesDao.likeComment(new CommentLikes(null,223L,233)));
-        System.out.println(commentLikesDao.unlikeComment(new CommentLikes(null,222L,333)));
+        //Category category = new Category();
+        //category.setCategoryName("test3");
+        //Category test = categoryDao.queryCategoryByName("test3");
+        //if (test != null){
+        //    System.out.println("test.getId() = " + test.getId());
+        //}else {
+        //    System.out.println("categoryDao = " + categoryDao.insertCategory(category));
+        //}
 
-        System.out.println(blogLikesDao.queryLike(new BlogLikes(null,22L,33)));
-        System.out.println(blogLikesDao.likeBlog(new BlogLikes(null,22L,33)));
-        System.out.println(blogLikesDao.unlikeBlog(new BlogLikes(null,22L,33)));*/
+        Blog blog = new Blog();
+        blog.setTitle("title");
+        blog.setMdContent("md");
+        blog.setHtmlContent("html");
+        blog.setSummary("summary");
+        blog.setUid(21);
+        blog.setPublishData(new Date(2000 - 1 - 1));
+        blog.setState(true);
+        blog.setPageView(100L);
+        blog.setLikeCount(100L);
+
+        System.out.println("before" + blog.getId());
+        System.out.println(blogDao.insertBlog(blog));
+        System.out.println("after" + blog.getId());
+
     }
 
 }
