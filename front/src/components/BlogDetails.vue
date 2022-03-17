18b26c3317f8cc9017c01901c9b9446a2abd4afe
Frode <15272516991@163.com>
2022-03-17 22:39:52 +0800

    博客详情接口

--- front/src/components/BlogDetails.vue
+++ front/src/components/BlogDetails.vue
@@ -2,34 +2,66 @@
   <div>
     <!-- <el-empty description="暂无数据"></el-empty>
      -->
-     <v-md-editor v-model="text" height="400px"></v-md-editor>
+    <v-md-preview :text="markdown"></v-md-preview>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+// main.js
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/preview.css';
+// 引入你所使用的主题 此处以 github 主题为例
+import githubTheme from '@kangc/v-md-editor/lib/theme/github';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
 
-import Vue from 'vue';
-import VueMarkdownEditor from '@kangc/v-md-editor';
-import '@kangc/v-md-editor/lib/style/base-editor.css';
-import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+// highlightjs
+import hljs from 'highlight.js';
 
-VueMarkdownEditor.use(vuepressTheme);
+import axios from 'axios'
+import bus from '@/components/eventBus.js'
 
-Vue.use(VueMarkdownEditor);
+
+VMdPreview.use(githubTheme, {
+  Hljs: hljs,
+});
+Vue.use(VMdPreview);
+
+
 
 export default {
 
-    
+
   name: 'BlogDetails',
   data () {
-      return {
-          text: '', 
-      }
+    return {
+      id: 1,
+      markdown: '## Blog'
+    }
 
+  },
+
+  methods: {
+    async getBlog () {
+      try {
+        const { data: res } = await axios.get('http://localhost:8080/blog/' + this.id + '')
+        if (res.code === 200) {
+          this.markdown = res.data
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created () {
+    bus.$on('getBlogId', id => {
+      this.id = id;
+      //   console.log(id);
+      getBlog(); // * 获取博客内容
+    })
   }
 }
 </script>
 
 <style>
-
 </style>
