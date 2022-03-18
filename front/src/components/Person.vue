c92442ff40ff4867710abdee908dd214f28caedf
Frode <15272516991@163.com>
2022-03-18 16:22:25 +0800

    个人中心

---
+++ front/src/components/Person.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-empty description="暂无数据"></el-empty>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Person',
+
+  data () {
+    return {
+
+    };
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
