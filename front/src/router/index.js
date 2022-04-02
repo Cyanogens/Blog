05c81cbad9e9c0b4b2d7ee90a0f38e29da63a020
Frode <15272516991@163.com>
2022-04-02 19:46:41 +0800

    补充拦截器判断条件

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -113,7 +113,7 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   // * 访问特定组件时，询问是否已经登录
   if (to.path == '/person' || to.path == '/editBlog') {
-    if (sessionStorage['token'] === 'F') {
+    if (sessionStorage['token'] === 'F' || sessionStorage['token'] == null) {
       console.log('未登录')
       // this.$message.error('请先登录')
       next('/login')
